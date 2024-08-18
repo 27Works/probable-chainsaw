cd6521ad10e163e6feca27a3490585f4feda35bd
cherrydub <cherrydub@gmail.com>
2024-08-18 18:12:15 +0100

    article post updated using hero banner + article author. Used html-react-parser however still need to space out the <p> content within each bodyText

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { notFound } from "next/navigation";
+import H1Gradient from "@/components/ui/H1Gradient";
+import Image from "next/image";
+import Link from "next/link";
+import parse from "html-react-parser";
 
 export default async function PostsSlugPage({
   params,
@@ -7,10 +11,6 @@ export default async function PostsSlugPage({
   params: { slug: string };
 }) {
   const { slug } = params;
-
-  console.log("slug ====>>>>", slug);
-
-  //radical-cup-back-at-barber-in-2020
 
   const response = await fetch(
     `https://caruuto.27.works/api/v1/posts?filter={"slug":"${slug}"}`,
@@ -27,19 +27,115 @@ export default async function PostsSlugPage({
 
   const data = await response.json();
 
-  //   console.log("response data ===>>>", data);
-
   if (!data.results || data.results.length === 0) {
     notFound();
   }
 
-  console.log("results ===>>>", data.results[0]);
-
   const post = data.results[0];
 
+  const getSection = (type: string) =>
+    post.sections.find((s: any) => s.content_type === type);
+
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
+
   return (
-    <>
-      <pre>{JSON.stringify(post, null, 2)}</pre>
-    </>
+    <div className="relative">
+      <div className="relative w-full h-[700px]">
+        <Image
+          src="/images/article_hero.jpg"
+          alt={post.title}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="opacity-50"
+          priority
+        />
+        <div className="absolute top-0 left-0 right-0 h-32 bg-gradient-to-b from-black to-transparent"></div>
+        <div className="absolute bottom-0 left-0 right-0 h-64 bg-gradient-to-t from-black to-transparent"></div>
+      </div>
+
+      <div className="relative max-w-[820px] border mx-auto -mt-64 text-white px-4 space-y-7">
+        {/* top section, home, latest, title summary */}
+        <section className="max-w-[715px] border">
+          <p className="text-sm">
+            <Link href="/" className="text-[#F2CB13]">
+              Home
+            </Link>{" "}
+            <Link href="/posts" className="text-[#F2CB13]">
+              / Latest News
+            </Link>{" "}
+          </p>
+          <div className="max-w-[715px] border pt-[11px] pb-[19px]">
+            <H1Gradient className="text-5xl font-futura-bold">
+              {parse(post.title.toUpperCase())}
+            </H1Gradient>
+          </div>
+          <p className="text-xl border max-w-[680px]">
+            {parse(getSection("subtitle")?.content)}
+          </p>
+        </section>
+        <div></div>
+        {/* avatar, name, date, 2 icons */}
+        <section className="flex items-center max-w-[680px] border">
+          <div className="flex items-center space-x-3 mr-3">
+            <Image
+              src="/images/article_author.jpg"
+              alt="Author"
+              width={42}
+              height={42}
+              className="rounded-full"
+            />
+            <span className="text-[16px] font-futura-book">
+              {post.createdBy || "John Smith"}
+            </span>
+          </div>
+          <span className="text-[#D9D9D9]">•</span>
+          <div className="text-[16px] ml-3">
+            <span className="text-[16px] font-futura-book">
+              {formatDate(post.created_at)}
+            </span>
+          </div>
+        </section>
+        {/* Content sections */}
+        {post.sections.map((section: any, index: number) => {
+          switch (section.content_type) {
+            case "bodyText":
+              return (
+                <section
+                  key={index}
+                  className="max-w-[680px] border text-[18px] font-futura-book text-[#B1B3B3]"
+                >
+                  {parse(section.content || "")}
+                </section>
+              );
+            case "pullQuote":
+              return (
+                <section key={index} className="max-w-[680px] border">
+                  <div className=" border-l-[5px] border-[#F2CB13] pl-[25px]">
+                    <H1Gradient className="font-futura-bold text-[24px] ">
+                      "{section.content.toUpperCase()}"
+                    </H1Gradient>
+                    {section.author && (
+                      <p className="mt-[7px] text-[#B1B3B3] text-[14px] font-futura-book">
+                        {section.author}
+                      </p>
+                    )}
+                  </div>
+                </section>
+              );
+
+            default:
+              return null;
+          }
+        })}
+      </div>
+    </div>
   );
 }
---
+++ app/posts/page.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function PostsPage() {
+  const response = await fetch(`https://caruuto.27.works/api/v1/posts/`, {
+    headers: {
+      Authorization: `Api-key-v1 ${process.env.APIKEY}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+
+  if (!data.results || data.results.length === 0) {
+    notFound();
+  }
+
+  const slugs = data.results.map((post: any) => {
+    return post.slug;
+  });
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      <ul className="flex-center-col">
+        {slugs.map((slug: string) => (
+          <li key={slug}>
+            <Link href={`/posts/${slug}`}>{slug}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
